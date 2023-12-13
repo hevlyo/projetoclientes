b6de8633af5f8762c142beabe72ae304b1987aa8
hevlyo <riquelme.pereira1@gmail.com>
2023-12-13 10:28:40 -0300

    Criando Projetinho Clientes

---
+++ src/models/clientModel.js
@@ -0,0 +1,40 @@
+const db = require('../../config/database.js');
+const { DataTypes } = require('sequelize');
+
+const Cliente = db.define('cliente', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    salario: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    endereco: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    cep: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    telefone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        },
+    },
+    });
+
+module.exports = Cliente;
